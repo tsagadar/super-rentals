69a73cca2f81b582752483ed122e139868ab245a
Marcel Müller <marcel.mueller@husqvarnagroup.com>
2024-03-14 18:22:14 +0100

    dotenv: automatically source .env when developing

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,8 +1,20 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const buildMode = EmberApp.env();
+
+function isProductionBuild() {
+  return buildMode === 'production';
+}
 
 module.exports = function (defaults) {
+  // Only load .env file if not in production environment
+  if (!isProductionBuild()) {
+    require('dotenv').config();
+  }
+
+  const prependUrl = "https://sentry.relar.ch/";
+
   const app = new EmberApp(defaults, {
     'ember-cli-terser': {
       enabled: true,
