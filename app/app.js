5090865e6f5395f20c1afe4f4266de4fad0784de
Marcel Müller <marcel.mueller@husqvarnagroup.com>
2024-03-14 16:47:45 +0100

    sentry: use captureConsoleIntegration for console.error capturing

--- app/app.js
+++ app/app.js
@@ -3,9 +3,11 @@ import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'super-rentals/config/environment';
 import * as Sentry from '@sentry/ember';
+import { captureConsoleIntegration } from '@sentry/integrations';
 
 Sentry.init({
   dsn: config.SENTRY_DSN,
+  integrations: [captureConsoleIntegration({levels:['error']})],
 });
 
 export default class App extends Application {
